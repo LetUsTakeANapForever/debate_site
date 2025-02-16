b58d7c0283594ba6f09427dee366d98a5a0b944d
LetUsTakeANapForever <kandamkankaw@gmail.com>
2025-02-16 22:22:05 +0700

    create sign up page

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const bcrypt = require("bcrypt");
+
+const app = express();
+
+app.set("view engine", "ejs");
+// static file
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("login");
+});
+
+app.get("/", (req, res) => {
+  res.render("signup");
+});
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server running on Port: ${port}`);
+});
